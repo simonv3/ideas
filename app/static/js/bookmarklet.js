616a5702ca02c60a155f119576468a2d9dcb2653
Simon Vansintjan <svansintjan@gmail.com>
2012-01-28 16:29:42 +0000

    edited the front page to contain some information

--- app/static/js/bookmarklet.js
+++ app/static/js/bookmarklet.js
@@ -22,14 +22,9 @@
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
 			// your JavaScript code goes here!
-            url = "http://localhost:8000/bookmarklet/idea/";
-            /*newwindow=window.open(url,'Idea Board','height=300,width=350');
-        	if (window.focus) {
-                newwindow.focus()
-            }*/
-            
-            $("body").append('<div id="idea_bookmarklet" style="width:80%; height:400px; text-align:center; position:fixed; top:10%; left:10%; z-index:1000; background-color:white; rgba:(240,240,240,0.25);">'
-                             +'<iframe id="the_frame" width="400" height="400" src="http://localhost:8000/bookmarklet/idea/" onload="$(\'#wikiframe iframe\').slideDown(500);">Enable iFrames.</iframe>'
+            url = "http://ideas.vansintjan.net/bookmarklet/idea/";
+             $("body").append('<div id="idea_bookmarklet" style="width:80%; height:400px; text-align:center; position:fixed; top:10%; left:10%; z-index:1000; background-color:white; rgba:(240,240,240,0.25);">'
+                             +'<iframe id="the_frame" width="400" height="400" src="http://ideas.vansintjan.net/bookmarklet/idea/" onload="$(\'#wikiframe iframe\').slideDown(500);">Enable iFrames.</iframe>'
                              +'<a href="" id="close_idea_bookmarklet">Close</a>'
                              +'</div>'
                             );
