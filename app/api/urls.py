3c47aabf7db146123ffefb40d5c369c0efe30fd7
Simon Vansintjan <svansintjan@gmail.com>
2012-07-02 15:56:57 -0700

    installed api

--- app/api/urls.py
+++ app/api/urls.py
@@ -5,7 +5,7 @@ from glossy_app.api.handlers import LanguageHandler
 language_handler = Resource(LanguageHandler)
 
 urlpatterns = patterns('',
-   url(r'^language/(?P<language_id>[^/]+)/', language_handler),
-   url(r'^languages/', language_handler),
+   url(r'^idea/(?P<language_id>[^/]+)/', language_handler),
+   url(r'^ideas/', language_handler),
 )
 
