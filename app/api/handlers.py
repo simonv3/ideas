45b98310946327ad5f191e1cd210f3d09a14434c
Simon Vansintjan <svansintjan@gmail.com>
2012-07-03 18:22:33 -0700

    expanded API to include API key and signature method.

--- app/api/handlers.py
+++ app/api/handlers.py
@@ -2,14 +2,41 @@ from piston.handler import BaseHandler
 from django.contrib.auth.models import  User
 from app.models import Idea
 
+from app.api.auth import key_check, secrets
+
+
 class UserHandler(BaseHandler):
     allowed_methods= ('GET',)
     model = User
+    fields = ('username','is_active','last_login','email','date_joined')
     def read(self, request, user_id=None):
-        base = User
+        base = User.objects
+        if user_id:
+            return base.get(pk=user_id)
+        else:
+            return {'error':'supply user information'}
+
+
+class UserIdeasHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    model = Idea
+    fields = ( ('user', ('id', 'username', 'email', 'date_joined', 'last_login')), 'idea', 'id', )
+    def read(self, request,  apikey, apisignature, user_id=None):
+        base = Idea.objects
+        query="/user/"+user_id+"/ideas/"
+        print user_id
+        if not key_check(apikey, apisignature,query):
+            return {'error':'authentication required'}
+        elif user_id:
+            return base.filter(user = user_id)
+        else:
+            return  {'error':'supply user information'}
+
 
 class IdeaHandler(BaseHandler):
     allowed_methods = ('GET',)
+    fields = ('id','idea','user','date')
+
     model = Idea
     def read(self, request, idea_id=None):
    
--- app/api/urls.py
+++ app/api/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from app.api.handlers import IdeaHandler
+from app.api.handlers import IdeaHandler, UserHandler, UserIdeasHandler
 
 idea_handler = Resource(IdeaHandler)
+user_handler = Resource(UserHandler)
+user_ideas_handler = Resource(UserIdeasHandler)
 
 urlpatterns = patterns('',
-   url(r'^idea/(?P<idea_id>[^/]+)/', idea_handler),
-   url(r'^ideas/', idea_handler),
+    url(r'^idea/(?P<idea_id>[^/]+)/', idea_handler),
+    url(r'^ideas/', idea_handler),
+
+    url(r'^user/(?P<user_id>[^/]+)/ideas/(?P<apikey>[^/]+)/(?P<apisignature>[^/]+)/', user_ideas_handler),
+    url(r'^user/(?P<user_id>[^/]+)/', user_handler),
 
     'piston.authentication',
     url(r'^oauth/request_token/$','oauth_request_token'),
     url(r'^oauth/authorize/$','oauth_user_auth'),
     url(r'^oauth/access_token/$','oauth_access_token'),
-)
-      
-   
+
 )
 
